f84c9a48c0dcb4577d05baed352ebf91c505244c
Bandana Kushwaha <72065598+bandana2315@users.noreply.github.com>
2022-08-20 14:59:03 +0530

    Time: 2616 ms (7.07%), Space: 6 MB (44.97%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        if(n==0)return 1;
+        else if(abs(x)==1 && n%2==0) return abs(x);
+        else if(abs(x)==1 && n%2!=0) return (x);
+        if(n>0){
+        while(n!=0){
+             ans=ans*x;
+            if(ans==0)return 0;
+            n--; 
+        }
+        }
+        else{
+            while(n!=0){
+            ans=(1/x)*ans;
+            n++;
+            }
+        }
+        return ans;
+    }
+};
