e72c1a44d2580c3b019a9111eb124e94e2a7a33a
Bandana Kushwaha <72065598+bandana2315@users.noreply.github.com>
2022-08-27 11:53:17 +0530

    Time: 35 ms (38.51%), Space: 19.7 MB (13.58%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       int count=1;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(n==1 ||n==2)return nums[0];
+       int n=nums.size();
+        int ans;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            if(nums[i]==nums[i+1])
-                count++;
-            else {
-                if(count>n/2)return nums[i];
-               count=1;
-            }
-            }
-        
-        return -1;
+            mp[nums[i]]++;
+        }
+        for(auto it:mp){
+            if(it.second>n/2)ans=it.first;
+        }
+        return ans;
     }
 };
